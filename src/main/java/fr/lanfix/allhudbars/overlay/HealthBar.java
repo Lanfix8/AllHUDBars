3b3eb9d44b3f7f617d5f175ee0c61d07c43c0efb
Lanfix <raphael.chapotard@outlook.fr>
2025-07-07 14:47:51 +0200

    Updated to 1.21.7

--- src/main/java/fr/lanfix/allhudbars/overlay/HealthBar.java
+++ src/main/java/fr/lanfix/allhudbars/overlay/HealthBar.java
@@ -37,7 +37,7 @@ public class HealthBar {
             renderAbsorptionBar(context, x, y, player);
             renderAbsorptionValue(textRenderer, context, x, y, player);
         }
-        // We need to render the health bar after beacause they overlap a little
+        // We need to render the health bar after because they overlap a little
         renderHealthBar(context, tickDelta, x, y, player);
         renderHealthValue(textRenderer, context, x, y, player);
     }
